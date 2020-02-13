9175294636f6ba969a7a5d2547d7c30d3e0b361b
Marnix van den Bent <marnix.van.den.bent@rabobank.nl>
2020-02-13 17:05:13 +0100

    Dist

--- dist/vp-controller.d.ts
+++ dist/vp-controller.d.ts
@@ -1,7 +1,24 @@
-import { EventHandler, HttpService, Message, Plugin } from 'universal-ledger-agent';
-import { ChallengeRequestSigner, VerifiablePresentationGenerator, VerifiablePresentationSigner } from 'vp-toolkit';
-import { VerifiableCredentialHelper } from './service/verifiable-credential-helper';
-import { AddressHelper } from './service/address-helper';
+/*
+ *  Copyright 2020 Coöperatieve Rabobank U.A.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import { EventHandler, HttpService, Message, Plugin } from 'universal-ledger-agent'
+import { ChallengeRequestSigner, VerifiablePresentationGenerator, VerifiablePresentationSigner } from 'vp-toolkit'
+import { VerifiableCredentialHelper } from './service/verifiable-credential-helper'
+import { AddressHelper } from './service/address-helper'
+
 /**
  * The VP Controller ULA plugin
  * ensures a correct issue/verify flow
@@ -9,8 +26,8 @@ import { AddressHelper } from './service/address-helper';
  * the correct dependencies.
  */
 export declare class VpController implements Plugin {
-    private _vpGenerator;
-    private _vpSigners;
+  private _vpGenerator
+  private _vpSigners
     private _challengeRequestSigners;
     private _httpService;
     private _vcHelper;
@@ -44,29 +61,33 @@ export declare class VpController implements Plugin {
      * @param {AddressHelper} _addressHelper
      * @param {number} _accountId
      */
-    constructor(_vpGenerator: VerifiablePresentationGenerator, _vpSigners: VerifiablePresentationSigner[], _challengeRequestSigners: ChallengeRequestSigner[], _httpService: HttpService, _vcHelper: VerifiableCredentialHelper, _addressHelper: AddressHelper, _accountId: number);
-    /**
-     * The name of the plugin
-     * @return {string}
-     */
-    readonly name: string;
-    /**
-     * The current wallet/profile ID
-     * @return {number}
-     */
-    /**
-    * When the user switches to a different
-    * wallet/profile, update the accountId
-    * to make sure the correct keys are
-    * generated and used.
-    */
-    accountId: number;
-    /**
-     * Receive the eventHandler so we can put messages
-     * back on the ULA again
-     * @param {EventHandler} eventHandler
-     */
-    initialize(eventHandler: EventHandler): void;
+    constructor (_vpGenerator: VerifiablePresentationGenerator, _vpSigners: VerifiablePresentationSigner[], _challengeRequestSigners: ChallengeRequestSigner[], _httpService: HttpService, _vcHelper: VerifiableCredentialHelper, _addressHelper: AddressHelper, _accountId: number);
+
+  /**
+   * The name of the plugin
+   * @return {string}
+   */
+  get name (): string;
+
+  /**
+   * The current wallet/profile ID
+   * @return {number}
+   */
+  get accountId (): number;
+  /**
+   * When the user switches to a different
+   * wallet/profile, update the accountId
+   * to make sure the correct keys are
+   * generated and used.
+   */
+  set accountId (id: number);
+
+  /**
+   * Receive the eventHandler so we can put messages
+   * back on the ULA again
+   * @param {EventHandler} eventHandler
+   */
+  initialize (eventHandler: EventHandler): void;
     /**
      * Handle incoming messages
      * @param {Message} message
