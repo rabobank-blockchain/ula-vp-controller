83002c5d8169f1e5256e8c18364df93b16b422ba
Jarl Nieuwenhuijzen <jarl.nieuwenhuijzen@gmail.com>
2020-07-24 19:14:38 +0200

    Upgrade ula-vc-data-management from 0.1.1 to 0.1.2

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { VpController } from './vp-controller';
-export { AddressHelper } from './service/address-helper';
-export { VerifiableCredentialHelper } from './service/verifiable-credential-helper';
